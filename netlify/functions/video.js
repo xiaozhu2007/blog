91410b7db7b2d2147d30f863fbf0df4140b7b41a
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-07-17 09:04:45 +0800

    add Fuctions

---
+++ netlify/functions/video.js
@@ -0,0 +1,42 @@
+var yt = require('youtube.get-video-info')
+
+exports.handler = function(event, context, callback) {
+  const videoID = event.queryStringParameters.v
+
+  if(!videoID) {
+    callback('Video ID is required', {statusCode: 500})
+    return
+  }
+
+  yt.retrieve(videoID, function(err, res) {
+    if (err) {
+      callback(err, {statusCode: 500})
+      return
+    } else if (res.status === 'fail') {
+      callback(res.reason, {statusCode: 500})
+      return
+    } else {
+      const playerResponse = JSON.parse(res.player_response)
+      const captions = playerResponse.captions.playerCaptionsTracklistRenderer.captionTracks.filter(c => c.kind !== 'asr')
+      const streams = playerResponse.streamingData.formats
+      const details = playerResponse.videoDetails
+
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        },
+        body: JSON.stringify({
+          captions,
+          streams,
+          details: {
+            title: details.title,
+            author: details.author,
+            videoId: details.videoId,
+          }
+        })
+      })
+    }
+  })
+}
