43ba8520d54835a0dfb89482ba424bcf6059aa0f
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-08-25 11:17:08 +0800

    修改sw.js (#18)
    * 修改sw.js
    
    Signed-off-by: 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
    
    * Update sw.js
    
    Signed-off-by: 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
    
    * Update sw.js
    
    Signed-off-by: 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
    
    * Update sw.js
    
    Signed-off-by: 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
    
    Signed-off-by: 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>

--- themes/pigfly/source/sw.js
+++ themes/pigfly/source/sw.js
@@ -1,4 +1,30 @@
+///// 定义 /////
 const CACHE_NAME = 'PigCDNCache';
+self.cons = {
+    s: (m) => {
+        console.log(`%c[SUCCESS]%c ${m}`, 'color:white;background:green;', '')
+    },
+    w: (m) => {
+        console.log(`%c[WARNING]%c ${m}`, 'color:brown;background:yellow;', '')
+    },
+    i: (m) => {
+        console.log(`%c[INFO]%c ${m}`, 'color:white;background:blue;', '')
+    },
+    e: (m) => {
+        console.log(`%c[ERROR]%c ${m}`, 'color:white;background:red;', '')
+    },
+    d: (m) => {
+        console.log(`%c[DEBUG]%c ${m}`, 'color:white;background:black;', '')
+    }
+}
+
+const generate_uuid = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
 let cachelist = [
     '/offline.html',
     'https://npm.elemecdn.com/chenyfan-os@0.0.0-r6'
@@ -31,11 +57,24 @@ self.addEventListener('message', (event) => {
 
 ///// 处理器 /////
 const handle = async (req) => {
-    const domain = req.url.split('/')[2];
+    const urlStr = req.url
+    let urlObj = new URL(urlStr)
+    const pathname = urlObj.href.substr(urlObj.origin.length)
+    const port = urlObj.port
+    const domain = (urlStr.split('/'))[2]
+    if (pathname.match(/\/sw\.js/g)) return fetch(req)
+    if (pathname.match('/cdn-cgi/')) return new Response(null, { status: 403 })
+    const path = pathname.split('?')[0]
+    const query = q => urlObj.searchParams.get(q)
+    const nqurl = urlStr.split('?')[0]
+    const nqreq = new Request(nqurl)
+    
+    if (query('nosw') == 'true') {
+        return fetch(req)
+    }
     if (domain.match("unpkg.com")) {
         return fetch(req.url.replace("https://unpkg.com", "https://zhimg.unpkg.com"));
-    }
-    else {
+    } else {
         return fetch(req)
     }
 }
@@ -44,3 +83,32 @@ const handleerr = async (req, msg) => {
     return new Response(`<h1>网站遇到了致命错误</h1>
     <b>${msg}</b>`, { headers: { "content-type": "text/html; charset=utf-8" } })
 }
+const handlecgi = async (req) => {
+    const intelligent_size = (byte) => {
+        if (byte < 1024) {
+            return `${byte}B`
+        } else if (byte < 1024 * 1024) {
+            return `${(byte / 1024).toFixed(2)}KB`
+        } else if (byte < 1024 * 1024 * 1024) {
+            return `${(byte / 1024 / 1024).toFixed(2)}MB`
+        } else {
+            return `${(byte / 1024 / 1024 / 1024).toFixed(2)}GB`
+        }
+    }
+    const urlStr = req.url
+    let urlObj = new URL(urlStr)
+    const pathname = urlObj.href.substr(urlObj.origin.length)
+    const query = q => urlObj.searchParams.get(q)
+    const endpoint = "https://cdn1.tianli0.top/npm/chenyfan-blog-helper-dash@0.0.9/"
+    let dash_main = await (await fetch(endpoint + 'index.html')).text()
+    let client_ip = await (await fetch('https://icanhazip.com/')).text()
+    return new Response(dash_main
+                .replace(/<!--IP-->/g, client_ip)
+                .replace(/<!--SECTION-->/g, '')
+                , {
+                    headers: {
+                        'Content-Type': 'text/html'
+                    }
+                })
+}
+
