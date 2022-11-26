99afd1df96567c292202bb081f376208f9fcd859
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-11-26 20:49:32 +0800

    小问题

---
+++ .github/token.js
@@ -0,0 +1,17 @@
+var tokens = [{
+    "name": "测试",
+    "site": "https://www.runoob.com",
+    token: "kgbnvPFYtH8RYxxRt3JPekmR5ChFhRL5"
+}, {
+    "name": "测试2",
+    "site": "http://#/",
+    token: "none"
+}, {
+    "name": "测试3",
+    "site": "https://www.baidu.com",
+    token: "gg.gg"
+}, {
+    "name": "个人",
+    "site": "https://liuzhen932.fun",
+    token: "45p2voRaswNADCG"
+}];
