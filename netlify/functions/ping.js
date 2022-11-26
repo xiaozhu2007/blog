b10b5578ca67e885be0cfdb205aca1b31fbd9a29
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-11-26 20:28:32 +0800

    同步API参数

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,8 +1,13 @@
 exports.handler = async function (event, context) {
+  if (event.queryStringParameters.msg != null) {
+    var custom_message = event.queryStringParameters.msg
+  } else {
+    var custom_message = 'Pong!'
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Pong!",
+      message: custom_message,
       code: 0
     }),
   };
