f5275d1c779a8c05ceb1f3ce36002056ccdb1e6b
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-11 15:35:22 +0800

    0

--- netlify/functions/dns-lookup.js
+++ netlify/functions/dns-lookup.js
@@ -5,11 +5,25 @@
  */
 const dns = require('dns');
 
+let hostname = event.queryStringParameters.hostname
+
 exports.handler = function (event, context, callback) {
-  dns.resolve(event.queryStringParameters.hostname, 'ANY', (err, records) => {
+  dns.resolve(hostname, 'ANY', (err, records) => {
     callback(err, {
       statusCode: 200,
       body: JSON.stringify(records),
     });
   });
 };
+
+function lookup(hostname) {
+  dns.lookup(hostname, function onLookup(err, address, family) {
+    // console.log(address);
+    dns.reverse(address, function (err, hostnames) {
+      if (err) {
+        console.log(err.stack);
+      }
+      // console.log('反向解析 ' + address + ': ' + JSON.stringify(hostnames));
+    });
+  });
+}
