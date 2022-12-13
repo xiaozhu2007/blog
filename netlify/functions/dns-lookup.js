8244039a0d9013ad301c24a4c3f415d5cc03cfb2
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-13 16:49:24 +0800

    New posts

--- netlify/functions/dns-lookup.js
+++ netlify/functions/dns-lookup.js
@@ -5,6 +5,17 @@
 const dns = require('dns');
 
 exports.handler = function (event, context, callback) {
+  dns.lookup(event.queryStringParameters.hostname, function onLookup(err, address, family) {
+
+  });
+  /*
+  dns.resolve(event.queryStringParameters.hostname, 'A', (err, hostnames) {
+    if (!err) {
+      return hostnames
+    } else {
+      return err
+    }
+  });*/
   dns.resolve(event.queryStringParameters.hostname, 'ANY', (err, records) => {
     callback(err, {
       statusCode: 200,
