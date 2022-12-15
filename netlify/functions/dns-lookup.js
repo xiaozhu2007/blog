69c836f556e86fa5e6068977a3cf5a3d78a79346
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-15 08:42:44 +0800

    测试默认值

--- netlify/functions/dns-lookup.js
+++ netlify/functions/dns-lookup.js
@@ -5,9 +5,8 @@
 const dns = require('dns');
 
 exports.handler = function (event, context, callback) {
-  let hostname = event.queryStringParameters.hostname
-  let types = event.queryStringParameters.type
-
+  let hostname = event.queryStringParameters.hostname || 'www.baidu.com'
+  let types = event.queryStringParameters.type || 'A'
   dns.resolve(hostname, types, (err, records) => {
     callback(err, {
       statusCode: 200,
